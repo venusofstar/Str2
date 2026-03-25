779297167138ea0880229372c3f0c1ebc24fb28a
venusofstar <sumidomikeangelo24@gmail.com>
2026-03-25 22:25:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 10000;
 
 // 🔥 Direct stream links
 const STREAMS = {
-  tapsilog: "https://dice-live-ap.akamaized.net/hls/live/2001903/300024-317970/playlist.m3u8?hdntl=exp=1774433578~acl=%2f*~id=0284c0c6-9186-4abe-83b2-fb617476c6e3~data=hdntl,dWlkPWc2eG1OSHwyNTNlMTAyNS0zNTkwLTQyY2MtYTU1NS03Nzc0ZDQ5NWU3OTMmaXA9MTEyLjIwMS45Ny4yMjEmZXhwPTE3NzQ0MzM2MDYmZWlkPTMwMDAyNCZjaWQ9ZGNlLnRhcGdvJm9pZD0zMjUmdHlwZT1MSVZF~hmac=33ea3d6981227aeeb125557f45faf2e52cbe6c2125a13d1a757062b1cd9c0e85",
-  tapsports: "https://dice-live-ap.akamaized.net/hls/live/2102504/220939-318306/playlist.m3u8?hdntl=exp=1774433578~acl=%2f*~id=0284c0c6-9186-4abe-83b2-fb617476c6e3~data=hdntl,dWlkPWc2eG1OSHwyNTNlMTAyNS0zNTkwLTQyY2MtYTU1NS03Nzc0ZDQ5NWU3OTMmaXA9MTEyLjIwMS45Ny4yMjEmZXhwPTE3NzQ0MzM2MDYmZWlkPTMwMDAyNCZjaWQ9ZGNlLnRhcGdvJm9pZD0zMjUmdHlwZT1MSVZF~hmac=33ea3d6981227aeeb125557f45faf2e52cbe6c2125a13d1a757062b1cd9c0e85"
+  tapsilog: "https://dice-live-ap.akamaized.net/hls/live/2001903/300024-317970/playlist.m3u8?hdntl=exp=1774534907~acl=/*~id=da815416-94fb-4e05-9cf2-a5b0ef8d1f2f~data=hdntl,dWlkPW5VTkxWZnw3ZjAxZWMxNS1hMTQwLTRlZDMtODE2My1jOGQ1ZjMwZjg0Y2YmaXA9MTEyLjIwMS45Ny4yMjEmZXhwPTE3NzQ1MzQ5MzYmZWlkPTMwMDAyNCZjaWQ9ZGNlLnRhcGdvJm9pZD0zMjUmdHlwZT1MSVZF~hmac=167c9be888486c849ee84acce5daacc36aea13bf186f89b0dd84c82a7ea0f757",
+  tapsports: "https://dice-live-ap.akamaized.net/hls/live/2102504/220939-318306/playlist.m3u8?hdntl=exp=1774534907~acl=/*~id=da815416-94fb-4e05-9cf2-a5b0ef8d1f2f~data=hdntl,dWlkPW5VTkxWZnw3ZjAxZWMxNS1hMTQwLTRlZDMtODE2My1jOGQ1ZjMwZjg0Y2YmaXA9MTEyLjIwMS45Ny4yMjEmZXhwPTE3NzQ1MzQ5MzYmZWlkPTMwMDAyNCZjaWQ9ZGNlLnRhcGdvJm9pZD0zMjUmdHlwZT1MSVZF~hmac=167c9be888486c849ee84acce5daacc36aea13bf186f89b0dd84c82a7ea0f757"
 };
 
 // 🔥 Short URL redirect
